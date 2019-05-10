67be4e87d5a335ea7c4eb25d450050a8c3659b0f
johanhbh <39012304+johanhbh@users.noreply.github.com>
2019-05-10 10:29:06 +0200

    Copy From other

--- GradeBook/GradeBooks/RankedGradeBooks.cs
+++ GradeBook/GradeBooks/RankedGradeBooks.cs
@@ -1,3 +1,56 @@
+using GradeBook.Enums;
+using GradeBook.GradeBooks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GradeBook.GradeBooks
+{
+    class RankedGradeBook : BaseGradeBook
+    {
+        public RankedGradeBook(string name) : base(name)
+        {
+            Type = GradeBookType.Ranked;
+        }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+            {
+                throw new InvalidCastException("Ranked-grading requires a minimum of 5 students to work");
+                
+            }
+
+            var threshold = (int)Math.Ceiling (Students.Count * 0.2);
+            var grades = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
+
+            if (grades[threshold - 1] <= averageGrade)
+                return 'A';
+            else if (grades[(threshold * 2) - 1] <= averageGrade)
+            {
+                return 'B';
+            
+            }
+            else if (grades[(threshold * 3) - 1] <= averageGrade)
+            {
+                return 'C';
+
+            }
+            else if (grades[(threshold * 4) - 1] <= averageGrade)
+            {
+                return 'D';
+
+            }
+            else
+            {
+                return 'F';
+            }
+        }
+    }
+}
+        
+/* 
 using System.Linq;
 using System;
 using GradeBook.Enums;
@@ -38,3 +91,4 @@ namespace GradeBook.GradeBooks
 
     }
 }
+*/
